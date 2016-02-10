185285cbf2ce5db209287a36cfd29554d6a0443c
Red Forks <redforks@gmail.com>
2016-02-10 10:11:47 +0800

    Create Exit/Now alias

---
+++ hal.go
@@ -0,0 +1,19 @@
+// Package hal separate depends on on real hardware and os, such as time, file,
+// and other os operations. Use hal package mainly for easier to do unit test.
+//
+// Nearly all members in this package is unprotected alias of standard go
+// functions, do not alter them unless inside unit tests.
+package hal
+
+import (
+	"os"
+	"time"
+)
+
+var (
+	// Exit is alias of os.Exit
+	Exit = os.Exit
+
+	// Now is alias of time.Now
+	Now = time.Now
+)
